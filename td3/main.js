1e798e37536c42dbb0fc7882ad6c22201bad83f9
jacob643 <jacobdorais25@hotmail.com>
2018-03-22 13:33:12 -0400

    JS sending message

--- td3/main.js
+++ td3/main.js
@@ -1,5 +1,14 @@
-$(document).ready = function () {}
+$(document)
+  .ready = function () {}
 var bill = new User("billy", "test123", "PimpBoy345")
 var jacob = new User("jacob", "test123", "jacob")
-var a = new Observable(bill)
+var active_user = new Observable(bill)
 var b = new Observable(jacob)
+
+function envoyer_message() {
+  var content = document.getElementById("input");
+  var m = new Message("onMessage", active_user.channel.id,
+    content.value, active_user.user.name, Date.now());
+  active_user.socket.send(JSON.stringify(m));
+  content.value = "";
+}
