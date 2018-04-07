41ad36d64ac1961949f0852c7ffdf553695032a9
jacob643 <jacobdorais25@hotmail.com>
2018-04-07 15:53:43 -0400

    name choice

--- td3/main.js
+++ td3/main.js
@@ -1,9 +1,15 @@
 var bill = new User("billy", "test123", "PimpBoy345")
 var jacob = new User("jacob", "test123", "jacob")
-var active_user = new Observable(bill)
+var active_user //= new Observable(bill)
 
 $(document)
   .ready(function () {
+    var name = prompt("YOUR NAME:", "")
+    if (name == null || name == "") {
+      name = "no_name"
+    }
+    bill = new User("billy", "test123", name);
+    active_user = new Observable(bill);
     $("#message-sender").bind("click", active_user.sendMessage())
     $("#message-liker").bind("click", active_user.sendLike())
     $('#message-writer').keypress(function (e) {
